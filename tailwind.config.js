05132bc2be4a898a43734c5038433a98ea3857eb
pavanbuddha <45193542+pavanbuddha@users.noreply.github.com>
2020-10-22 10:55:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    mode: 'all',
+    enabled: true,
+    preserveHtmlElements: false,
+    content: ["./layouts/**/*.html"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
